de85c67e6ccb86a216572cd90710788686cc976c
v.markus <v.markus@tinkoff.ru>
2018-02-20 22:04:43 +0300

    Update gradle

--- app/src/main/java/com/source/subscity/api/entities/movie/Movie.kt
+++ app/src/main/java/com/source/subscity/api/entities/movie/Movie.kt
@@ -22,7 +22,7 @@ data class Movie(@SerializedName("id") @PrimaryKey val id: Long,
                  @SerializedName("genres") val genres: List<String>,
                  @SerializedName("languages") val languages: List<String>,
                  @SerializedName("poster") val poster: String,
-                 @SerializedName("rating") @Embedded(prefix = "rating_")val rating: Ratings,
+                 @SerializedName("rating") @Embedded(prefix = "rating_") val rating: Ratings,
                  @SerializedName("screenings") @Embedded(prefix = "screenings_") val screenings: Screenings,
                  @SerializedName("title") @Embedded(prefix = "title_") val title: Titles,
                  @SerializedName("trailer") @Embedded(prefix = "trailer_") val trailer: Trailers,
@@ -32,4 +32,11 @@ data class Movie(@SerializedName("id") @PrimaryKey val id: Long,
     constructor() : this(0, 0, emptyList(), emptyList(), DateTime(0), "",
             emptyList(), 0, emptyList(), emptyList(), "", Ratings(), Screenings(),
             Titles(), Trailers(), 0)
+
+    val commonRaiting: Double
+        get() {
+            val sum = rating.imdb.rating + rating.kinopoisk.rating
+            val count = if (rating.imdb.rating != 0.0 && rating.kinopoisk.rating != 0.0) 2 else 1
+            return sum / count
+        }
 }
