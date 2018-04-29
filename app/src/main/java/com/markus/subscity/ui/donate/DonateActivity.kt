469dd8328fb6c6808c42245edcdb8da824c23a32
v.markus <v.markus@tinkoff.ru>
2018-04-29 22:19:37 +0300

    Add billing

--- app/src/main/java/com/markus/subscity/ui/donate/DonateActivity.kt
+++ app/src/main/java/com/markus/subscity/ui/donate/DonateActivity.kt
@@ -3,8 +3,14 @@ package com.markus.subscity.ui.donate
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.util.Log
 import com.anjlab.android.iab.v3.BillingProcessor
 import com.anjlab.android.iab.v3.TransactionDetails
+import com.markus.subscity.R
+import com.markus.subscity.controllers.ContentLoadingController
 import com.markus.subscity.extensions.toast
 import com.markus.subscity.utils.BaseActivity
 
@@ -14,9 +20,16 @@ import com.markus.subscity.utils.BaseActivity
 class DonateActivity : BaseActivity(), BillingProcessor.IBillingHandler {
 
     private lateinit var billingProcessor: BillingProcessor
+    private lateinit var loadingController: ContentLoadingController
+
+    private lateinit var donatesList: RecyclerView
 
     companion object {
         const val LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAi5lzOD4D0Lg4vPUhX+qb5HWhDb66fa0+i6ginNFkWFRH1dzx9Nv6++LwfzpF1UiAJIDiS7jAllDfssgqF8a67XXgAUJ/c62sLLvhD/XO9ZhMxNQA+FMR3SZW/yj4AXi7NNz1dT/mU9wmOeJXbetWTsqG+GI3Ww25+E4WH8AqCQ9WpXH5oIkOFJpN6PHob80g5rMIXHMxdUbfHCfXwz3f9XTGtzkMFz81hyT45pgfa4iggsjkuEVHzMX9W42TQmhb/SEgaPBw8vUa905OVDvr40WRw6oCwa/u4XA6WTViDBP0EiZ3KvV6Z267Gd7zlwJJbYfXv070mgoEKDfH3qUxoQIDAQAB"
+        const val PRODUCT_TEA = "donate_tea"
+        const val PRODUCT_COFFEE = "donate_coffee"
+        const val PRODUCT_MOVIE_TICKET = "donate_movie_ticket"
+
 
         fun start(context: Context) {
             val intent = Intent(context, DonateActivity::class.java)
@@ -26,25 +39,25 @@ class DonateActivity : BaseActivity(), BillingProcessor.IBillingHandler {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                    .add(android.R.id.content, DonateFragment.newInstance())
-//                    .commit()
-//        }
-
-        billingProcessor = BillingProcessor(this, DonateFragment.LICENSE_KEY, this)
+        setContentView(R.layout.fragment_donate)
+        initToolbar()
+        donatesList = findViewById(R.id.rv_list)
+        loadingController = ContentLoadingController(this, R.id.rv_list, R.id.pb_progress)
+        billingProcessor = BillingProcessor(this, LICENSE_KEY, this)
+        loadingController.setContentState(ContentLoadingController.State.PROGRESS)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
     override fun onBillingInitialized() {
-        toast("SUCCESS")
-        val listOwnedProducts = billingProcessor.getPurchaseListingDetails("donate_tea")
+        val details = billingProcessor.getPurchaseListingDetails(ArrayList(listOf(PRODUCT_TEA, PRODUCT_COFFEE, PRODUCT_MOVIE_TICKET)))
+        donatesList.layoutManager = LinearLayoutManager(this)
+        donatesList.adapter = DonateAdapter(details.sortedBy { it.priceLong }) { billingProcessor.purchase(this, it.productId) }
+        loadingController.switchState(ContentLoadingController.State.CONTENT)
     }
 
     override fun onPurchaseHistoryRestored() {
@@ -52,10 +65,22 @@ class DonateActivity : BaseActivity(), BillingProcessor.IBillingHandler {
     }
 
     override fun onProductPurchased(productId: String, details: TransactionDetails?) {
-        toast("PURCHASED")
+        val string = "ProductId = $productId; Details = ${details?.toString()}"
+        Log.e("PURCHASED", string)
+        toast("PURCHASED __ $string")
     }
 
     override fun onBillingError(errorCode: Int, error: Throwable?) {
-        toast("ERROR")
+        val string = "ErrorCode = $errorCode; Error = ${error?.toString()}"
+        Log.e("ERROR", string)
+        toast("ERROR __ $string")
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }
---
+++ app/src/main/java/com/markus/subscity/ui/donate/DonateAdapter.kt
@@ -0,0 +1,45 @@
+package com.markus.subscity.ui.donate
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.anjlab.android.iab.v3.SkuDetails
+import com.markus.subscity.R
+
+/**
+ * @author Vitaliy Markus
+ */
+class DonateAdapter(private val details: List<SkuDetails>,
+                    private val clickListener: (SkuDetails) -> Unit) : RecyclerView.Adapter<DonateAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val item = LayoutInflater.from(parent.context).inflate(R.layout.item_donate, parent, false)
+        return ViewHolder(item)
+    }
+
+    override fun getItemCount(): Int {
+        return details.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(details[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private lateinit var sku: SkuDetails
+        private val title = view.findViewById<TextView>(R.id.tv_title)
+        private val price = view.findViewById<TextView>(R.id.tv_price)
+
+        init {
+            view.setOnClickListener { clickListener.invoke(sku) }
+        }
+
+        fun bind(sku: SkuDetails) {
+            this.sku = sku
+            title.text = sku.description
+            price.text = sku.priceText
+        }
+    }
+}
