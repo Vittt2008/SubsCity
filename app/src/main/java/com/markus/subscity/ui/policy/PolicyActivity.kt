0031f9f6d8aa2f18890d386e867441f6b70c3b00
Vitalii.Markus <vitalii.markus@revolut.com>
2019-06-17 00:44:05 +0300

    Add Dark Privacy Policy

--- app/src/main/java/com/markus/subscity/ui/policy/PolicyActivity.kt
+++ app/src/main/java/com/markus/subscity/ui/policy/PolicyActivity.kt
@@ -3,6 +3,9 @@ package com.markus.subscity.ui.policy
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import com.markus.subscity.R
 import com.markus.subscity.utils.BaseActivity
@@ -25,6 +28,21 @@ class PolicyActivity : BaseActivity() {
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         setContentView(R.layout.activity_policy)
         val policyView = findViewById<WebView>(R.id.wv_policy)
-        policyView.loadUrl("file:///android_asset/privacy_policy.html");
+        policyView.loadUrl(getString(R.string.policy_url))
+        policyView.webChromeClient = object : WebChromeClient() {
+
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                if (newProgress == 100) {
+                    view.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
